ab43189b42309db864e4fe0465263bd3e88728a5
Ponnu101 <144668800+PonnuAugustine@users.noreply.github.com>
2024-04-01 11:45:42 +0530

    Add files via upload

---
+++ EmpMain.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Employee{
+    int eNo, eSalary;
+    String eName;   
+
+    public Employee(int no, String name, int salary) {
+        this.eNo = no;
+        this.eName = name;
+        this.eSalary = salary;
+    }
+}
+class EmpMain{
+    public static void main(String arg[]){
+        System.out.println("PONNU AUGUSTINE");
+	System.out.println("23MCA044");
+	System.out.println("01/04/24");
+        Scanner scanner = new Scanner(System.in);       
+        System.out.print("Enter the no of Employees : ");
+        int n = scanner.nextInt();
+        Employee arr[] = new Employee[n];
+        for(int i=0; i<n; i++){
+            System.out.println("Enter the details of employee " + Integer.toString(i+1));
+            System.out.print("Enter the employee id : ");
+            int no = scanner.nextInt();
+            System.out.print("Enter the employee name : ");
+            scanner.nextLine();
+            String name = scanner.nextLine();
+            System.out.print("Enter the salary : ");
+            int salary = scanner.nextInt();
+            arr[i] = new Employee(no, name, salary);
+        }
+        System.out.print("\nEnter the employee id to search : ");
+        int id = scanner.nextInt();
+        int flag = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i].eNo == id){
+                System.out.println("Employee found.\nid : " + Integer.toString(id) + "\nname : " + arr[i].eName + "\nsalary : " + Integer.toString(arr[i].eSalary));
+                flag = 1;
+                break;
+            }
+        }
+        if(flag == 0) System.out.println("Employee not found");
+
+        scanner.close();
+    }
+}
